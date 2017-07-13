9853a43197bcc4d08bf92c3c95626196322a37f4
fitz.chen <fitz.chen@esenyun.com>
2017-07-13 16:29:08 +0800

    自用

--- src/defaultwidgets/object/object.widget.ts
+++ src/defaultwidgets/object/object.widget.ts
@@ -5,9 +5,11 @@ import { ObjectLayoutWidget } from '../../widget';
 @Component({
   selector: 'sf-form-object',
   template: `<fieldset *ngFor="let fieldset of formProperty.schema.fieldsets">
+<div class="row">
 	<legend *ngIf="fieldset.title">{{fieldset.title}}</legend>
-	<div *ngFor="let fieldId of fieldset.fields">
+	<div *ngFor="let fieldId of fieldset.fields" class="col-6 form-group">
 		<sf-form-element [formProperty]="formProperty.getProperty(fieldId)"></sf-form-element>
+	</div>
 	</div>
 </fieldset>`
 })
